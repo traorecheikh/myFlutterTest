d41cf32adc9b4a0b6293c8190c7d649d770535a9
traorecheikh <atidiane741@gmail.com>
2024-12-27 18:37:43 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/example/prototype_zeta/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.prototype_zeta
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+        ),
+        body: Dashboard(),
+      ),
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+  
+}
